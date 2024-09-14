06e6a128841ce7edd022d38a761506ccc37cefd4
Amulya Baniya <ronaldoamulya@gmail.com>
2024-09-14 15:08:10 +0100

    feat: added types

---
+++ SpectraDB/types.py
@@ -0,0 +1,9 @@
+from typing import Union, Iterable
+from spectradb.dataloaders import FluorescenceDataLoader, FTIRDataLoader, NMRDataLoader
+
+FluorescenceIterable = Iterable[FluorescenceDataLoader]
+FTIRIterable = Iterable[FTIRDataLoader]
+NMRIterable = Iterable[NMRDataLoader]
+
+DataLoaderIterable = Union[FluorescenceIterable, FTIRIterable, NMRIterable]
+DataLoaderType = Union[FluorescenceDataLoader, FTIRDataLoader, NMRDataLoader]
